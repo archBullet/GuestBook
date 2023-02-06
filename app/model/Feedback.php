442c191a91eebf4ddf7922bd0bad11140e40bbb2
archBullet <bullet0593@gmail.com>
2023-02-06 17:29:50 +0500

    delete unuse methods from Feedback model

--- app/model/Feedback.php
+++ app/model/Feedback.php
@@ -2,49 +2,15 @@
 
 
 class Feedback {
-	private string $time;
-	private string $name;
-	private string $content;
 	private array $feedback = [];
 
 	public function __construct(string $name, string $content) {
-		$this->time = date('d.m.Y H:i:s', time());
-		$this->name = $name;
-		$this->content = $content;
 		$this->feedback['name'] = $name;
 		$this->feedback['content'] = $content;
 		$this->feedback['date'] = date('d.m.Y H:i:s', time());
 	}
 
 	public function getFeedback(): array {
-		// $this->feedback['id'] = $id;
 		return $this->feedback;
-	}
-
-	public function getTime(): string {
-		return $this->time;
-	}
-	
-	public function setTime(string $time): self {
-		$this->time = $time;
-		return $this;
-	}
-
-	public function getName(): string {
-		return $this->name;
-	}
-
-	public function setName(string $name): self {
-		$this->name = $name;
-		return $this;
-	}
-
-	public function getContent(): string {
-		return $this->content;
-	}
-
-	public function setContent(string $content): self {
-		$this->content = $content;
-		return $this;
 	}
 }
